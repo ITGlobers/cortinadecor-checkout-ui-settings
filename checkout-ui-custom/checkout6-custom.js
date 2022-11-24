f8381246c664187750c5b1db11c8c17b4ff50794
Sebastian G S <sebastian.garcia@itglobers.com>
2022-11-24 20:11:05 -0300

    se agregan estilos de header y footer para el checkout

---
+++ checkout-ui-custom/checkout6-custom.js
@@ -0,0 +1,40 @@
+
+let steps = {
+  currentStep: "",
+  allSteps: "",
+  init: function(){
+    setTimeout(function(){
+      steps.currentStep = location.hash.split('/')[1];
+      steps.currentStep = (steps.currentStep === "email") ? "profile" : steps.currentStep;
+      steps.allSteps = document.querySelectorAll('.buySteps ul li');
+      steps.setEvent();
+      steps.setClasses();
+    },500);
+  },
+  setEvent: function(){
+    addEventListener('hashchange',(ev) => {
+      steps.currentStep = ev.newURL.split('#/')[1];
+      steps.currentStep = (steps.currentStep === "email") ? "profile" : steps.currentStep;
+      steps.setClasses();
+    })
+  },
+  setClasses: function(){
+    console.log(this)
+    let setCurrentClass = false;
+    for(let i = this.allSteps.length -1 ; i >= 0; i--){
+      if(!setCurrentClass){
+        if(this.allSteps[i].classList.contains(this.currentStep+'Step')){
+          console.log("entre por verdadero");
+          setCurrentClass = true;
+          i++;
+        }else{
+          this.allSteps[i].classList.remove('current');
+        }
+      }else{
+        this.allSteps[i].classList.add('current');
+      }
+    }
+  }
+}
+
+steps.init();
